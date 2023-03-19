dd7a54da1dfa2080100bcbd8459ee5fe35dfde21
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-03-19 13:26:00 +0530

    create Addressbook Table

--- AddressBook service.sql
+++ AddressBook service.sql
@@ -1,4 +1,15 @@
 create database addressbook_service;
 use addressbook_service;
 show databases;
-
+create table AddressBook(
+						firstname varchar(50) not null,
+                        secondname varchar(50) not null,
+                        Address text not null,
+                        city varchar(30) not null,
+                        state varchar(30) not null,
+                        zip int not null,
+                        phoneNumber Long not null ,
+                        email text not null
+                        );
+select * from addressbook;
+	
